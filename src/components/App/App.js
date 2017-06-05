793c3bc9f1772a61035adf25305aa9edab4853ac
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-04 21:45:40 -0400

    components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
+import "./App.less"
+
 import React, { Component } from "react"
+
+import Project from "Project"
 
 export default class App extends Component {
 
   render() {
     return (
       <div className="App">
-        app
+        <Project id="1" />
       </div>
     )
   }
