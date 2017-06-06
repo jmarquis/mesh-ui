43fae74a3b6fbfcd8bd892f598d10be8314d7e3d
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-05 21:53:53 -0400

    the rest of the main components

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -2,12 +2,16 @@ import "./Collection.less"
 
 import React, { Component } from "react"
 
+import NoteList from "NoteList"
+import NoteEditor from "NoteEditor"
+
 export default class Collection extends Component {
 
   render() {
     return (
       <div className="Collection">
-        collection
+        <NoteList />
+        <NoteEditor />
       </div>
     )
   }
---
+++ src/components/NoteList/NoteList.js
@@ -0,0 +1,15 @@
+import "./NoteList.less"
+
+import React, { Component } from "react"
+
+export default class NoteList extends Component {
+
+  render() {
+    return (
+      <div className="NoteList">
+        note list
+      </div>
+    )
+  }
+
+}
