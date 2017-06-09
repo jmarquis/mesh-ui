cad004a1fcb07bced940b0d7d0a60a0c4c2857a3
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-08 23:08:02 -0400

    lists......list

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,12 +4,31 @@ import React, { Component } from "react"
 
 import SpaceSelector from "components/SpaceSelector"
 
+import ListIcon from "icons/list"
+
 export default class Menu extends Component {
 
   render() {
     return (
       <div className="Menu">
         <SpaceSelector />
+        <ol>
+          <li>
+            <a href="#"><ListIcon />story ideas</a>
+          </li>
+          <li>
+            <a href="#"><ListIcon />to do</a>
+          </li>
+          <li>
+            <a href="#"><ListIcon />journal</a>
+          </li>
+          <li>
+            <a href="#" className="active"><ListIcon />random lists</a>
+          </li>
+          <li>
+            <a href="#"><ListIcon />interview notes</a>
+          </li>
+        </ol>
       </div>
     )
   }
