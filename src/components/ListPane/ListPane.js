74156e209ec791d048fe85fa2bcd5870d57b8d22
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-13 22:45:59 -0400

    routing

--- src/components/ListPane/ListPane.js
+++ src/components/ListPane/ListPane.js
@@ -1,20 +1,29 @@
 import "./ListPane.less"
 
 import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { withRouter } from "react-router"
+import { NavLink } from "react-router-dom"
 import autobind from "autobind-decorator"
 
 import SearchField from "components/SearchField"
 
 import AddIcon from "icons/plus"
 
+@withRouter
 @autobind
 export default class ListPane extends Component {
+
+  static propTypes = {
+    match: PropTypes.object
+  }
 
   state = {
     searchFieldValue: ""
   }
 
   render() {
+    const { match } = this.props
     const { searchFieldValue } = this.state
     return (
       <section className="ListPane">
@@ -30,22 +39,22 @@ export default class ListPane extends Component {
 
         <ol>
           <li>
-            <a href="#">
+            <NavLink to={`${match.url}/1`}>
               <h3>Upcoming trips</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Qua tu etiam inprudens…</p>
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a href="#">
+            <NavLink to={`${match.url}/2`}>
               <h3>Upcoming trips</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Qua tu etiam inprudens…</p>
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a href="#">
+            <NavLink to={`${match.url}/3`}>
               <h3>Upcoming trips</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Qua tu etiam inprudens…</p>
-            </a>
+            </NavLink>
           </li>
         </ol>
 
