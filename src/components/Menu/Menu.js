2dfef74a775d5d2d2c5a662fcb2b1c4c52a3d31e
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-13 22:27:56 -0400

    list link routes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,32 +1,41 @@
 import "./Menu.less"
 
 import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { withRouter } from "react-router"
+import { NavLink } from "react-router-dom"
 
 import SpaceSelector from "components/SpaceSelector"
 
 import ListIcon from "icons/list"
 
+@withRouter
 export default class Menu extends Component {
 
+  static propTypes = {
+    match: PropTypes.object
+  }
+
   render() {
+    const { spaceId } = this.props.match.params
     return (
       <div className="Menu">
         <SpaceSelector />
         <ol>
           <li>
-            <a href="#"><ListIcon />story ideas</a>
+            <NavLink to={`/${spaceId}/1`}><ListIcon />story ideas</NavLink>
           </li>
           <li>
-            <a href="#"><ListIcon />to do</a>
+            <NavLink to={`/${spaceId}/2`}><ListIcon />to do</NavLink>
           </li>
           <li>
-            <a href="#"><ListIcon />journal</a>
+            <NavLink to={`/${spaceId}/3`}><ListIcon />journal</NavLink>
           </li>
           <li>
-            <a href="#" className="active"><ListIcon />random lists</a>
+            <NavLink to={`/${spaceId}/4`}><ListIcon />random lists</NavLink>
           </li>
           <li>
-            <a href="#"><ListIcon />interview notes</a>
+            <NavLink to={`/${spaceId}/5`}><ListIcon />interview notes</NavLink>
           </li>
         </ol>
       </div>
