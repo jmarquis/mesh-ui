f9595d5c4c20c394417129e5f5a99892f5ba9da8
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-13 22:19:44 -0400

    route based spaces

--- src/components/SpaceSelector/SpaceSelector.js
+++ src/components/SpaceSelector/SpaceSelector.js
@@ -1,35 +1,38 @@
 import "./SpaceSelector.less"
 
 import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { withRouter } from "react-router"
 import autobind from "autobind-decorator"
 
 import CaretIcon from "icons/caret-down"
 
+@withRouter
 @autobind
 export default class SpaceSelector extends Component {
 
-  state = {
-    selectedSpace: "1234"
+  static propTypes = {
+    match: PropTypes.object,
+    history: PropTypes.object
   }
 
   render() {
-    const { selectedSpace } = this.state
+    const selectedSpace = this.props.match.params.spaceId
     return (
       <div className="SpaceSelector">
-        {selectedSpace === "1234" ? "HRIS" : "Payments"}
+        {selectedSpace === "a1234" ? "HRIS" : "Payments"}
         <CaretIcon />
         <select value={selectedSpace} onChange={this.handleChange}>
-          <option value="1234">HRIS</option>
-          <option value="5678">Payments</option>
+          <option value="a1234">HRIS</option>
+          <option value="a5678">Payments</option>
         </select>
       </div>
     )
   }
 
   handleChange(event) {
-    this.setState({
-      selectedSpace: event.target.value
-    })
+    const { history } = this.props
+    history.push(`/${event.target.value}`)
   }
 
 }
