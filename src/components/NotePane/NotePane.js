b38cc2fec6cc4137dcb8be64eadb43d074c6cc26
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-18 17:16:50 -0400

    working note pane

--- src/components/NotePane/NotePane.js
+++ src/components/NotePane/NotePane.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types"
 
+import { connect } from "react-redux"
+
+@connect((state, props) => {
+
+  const { notes } = state
+  const { match: { params: { noteId } } } = props
+  const note = notes[noteId]
+
+  return {
+    note
+  }
+
+})
 export default class NotePane extends Component {
 
+  static propTypes = {
+    note: PropTypes.object
+  }
+
   render() {
+    const { note } = this.props
+    if (!note) return null
     return (
       <section className="NotePane">
-        note editor
+        {note.body}
       </section>
     )
   }
