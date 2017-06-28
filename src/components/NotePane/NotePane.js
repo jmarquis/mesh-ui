1c8764b9a9fe758269c06caf8e4a91a197da51a1
Jeremy Marquis <jeremy@jeremymarquis.com>
2017-06-27 22:02:29 -0400

    blank editor

--- src/components/NotePane/NotePane.js
+++ src/components/NotePane/NotePane.js
@@ -2,8 +2,9 @@ import "./NotePane.less"
 
 import React, { Component } from "react"
 import PropTypes from "prop-types"
-
 import { connect } from "react-redux"
+import autobind from "autobind-decorator"
+import { Editor, EditorState } from "draft-js"
 
 @connect((state, props) => {
 
@@ -16,10 +17,15 @@ import { connect } from "react-redux"
   }
 
 })
+@autobind
 export default class NotePane extends Component {
 
   static propTypes = {
     note: PropTypes.object
+  }
+
+  state = {
+    editorState: EditorState.createEmpty()
   }
 
   render() {
@@ -27,10 +33,15 @@ export default class NotePane extends Component {
     if (!note) return null
     return (
       <section className="NotePane">
-        <header>{note.title}</header>
-        {note.body}
+        {/* <header>{note.title}</header>
+        {note.body} */}
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
       </section>
     )
+  }
+
+  onChange(editorState) {
+    this.setState({ editorState })
   }
 
 }
